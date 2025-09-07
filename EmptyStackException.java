c6175bd9c06e9a48ffab259fa41e94e762dd063c
Kareem Moussa <kareemahmedabdulhafezmohamed.moussa@studenti.unipd.it>
2025-09-08 00:08:49 +0300

    Update EmptyStackException.java
    edit

--- EmptyStackException.java
+++ EmptyStackException.java
@@ -1,7 +1,4 @@
-/*
-   Foundations of Computer Science
-   Programming Test - 1 February 2023
-*/
+
 /** 
    Defines the Exception for an Empty stack
    Thrown by methods in the Stack class to indicate that the stack is empty
